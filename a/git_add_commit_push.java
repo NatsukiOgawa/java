340c0d3622137e434ac8941febd1fc03c463a888
NatsukiOgawa <ogawanatsuki19960701@gmail.com>
2020-02-10 17:21:27 +0900

    a

--- a/git_add_commit_push.java
+++ a/git_add_commit_push.java
@@ -3,7 +3,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Main {
+public class git_add_commit_push{
     public static void main (String args[]) {
 			try {
 					Process process = new ProcessBuilder("git", "add", "*").start();
